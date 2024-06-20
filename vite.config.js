1c5c05c749f105f02b14e9ad69d3e58fb6c47427
Nicolas <nicolas@naussa.com.uy>
2024-06-20 14:38:31 +0200

    fix jsbootstrap

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir: 'dist'
-  }
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: [
+        'node_modules/@popperjs/core/dist/umd/popper.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+      ]
+    }
+  },
+  base: './'
 })
